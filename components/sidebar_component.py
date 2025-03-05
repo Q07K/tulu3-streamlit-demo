72f465e8e1ee729eb270159c413054931e1274a5
gyuhyeong <kgh0730@gmail.com>
2025-03-06 00:23:45 +0900

    Refactor: expander추가 및 model setting expander 내부로 이동

--- components/sidebar_component.py
+++ components/sidebar_component.py
@@ -1,17 +1,29 @@
 import streamlit as st
 
+from widgets.chat_history_widget import history_block
 from widgets.slider_widget import hyperparameter_slider
 
 
 def sidebar():
     with st.sidebar:
-        st.subheader(body="Models")
-        st.selectbox(
-            label="Model Name",
-            options=["Tülu3-8B", "Tülu3-70B"],
-            key="model_name",
-        )
-        st.subheader(body="Hyperparameters")
-        hyperparameter_slider(name="Max Token")
-        hyperparameter_slider(name="Temperature")
-        hyperparameter_slider(name="Top K")
+        with st.expander(label="Model Settings", icon="⚙️"):
+            st.subheader(body="Model")
+            st.selectbox(
+                label="Model Name",
+                options=["Tülu3-8B", "Tülu3-70B"],
+                key="model_name",
+                label_visibility="collapsed",
+            )
+            st.subheader(body="Hyperparameters")
+            hyperparameter_slider(name="Max Token")
+            hyperparameter_slider(
+                name="Temperature", min_value=0.0, max_value=1.0
+            )
+            hyperparameter_slider(name="Top K")
+
+            st.subheader(body="Prompt")
+            st.text_area(label="system prompt")
+
+        st.subheader(body="Chat history")
+        for i in range(10):
+            history_block(title=str(i))
